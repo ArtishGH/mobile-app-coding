bd84fbb6a69450afc02c668969c993ab481806e9
ArtishGH <b.prejs06@gmail.com>
2023-10-05 09:11:50 +0200

    Working on AlbumView

--- PeachMusic/Views/AlbumView.tsx
+++ PeachMusic/Views/AlbumView.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, Image, Button, Modal, StyleSheet, ImageBackground } from 'react-native';
 import { Album } from './Song';
 import CustomSlider from './CustomSlider';
+import Icon from 'react-native-vector-icons/MaterialIcons'; // Import the icon component
+import Sound from 'react-native-sound'
 
 interface AlbumDetailsViewProps {
   album: Album | null;
@@ -23,15 +25,21 @@ export const AlbumDetailsView: React.FC<AlbumDetailsViewProps> = ({ album, onClo
           {album && (
             <View>
               <Image source={{ uri: album.albumImage }} style={{ width: 350, height: 350, marginHorizontal: 50, marginTop: 50, borderRadius: 10 }}/>
-              <Text style={{ marginHorizontal: 50, marginTop: 20, fontSize: 24 }}>{album.albumTitle}</Text>
-              <Text style={{ marginHorizontal: 50, marginTop: 5, fontSize: 20, color: 'gray' }}>{album.artist}</Text>
+              <Text style={{ marginHorizontal: 50, marginTop: 20, fontSize: 24, color: 'white' }}>{album.albumTitle}</Text>
+              <Text style={{ marginHorizontal: 50, marginTop: 5, fontSize: 20, color: 'lightgray' }}>{album.artist}</Text>
               <CustomSlider
                 value={sliderValue}
                 onValueChange={(newValue) => setSliderValue(newValue)}
               />
               {/* Add more album details here */}
-              <View style={{ margin: 10 }}>
+              <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                <Icon name="skip-previous" size={40} color="white"/>
+                <Icon name="pause" size={40} color="white"/>
+                <Icon name="skip-next" size={40} color="white"/>
+              </View>
+              <View style={{ margin: 10, alignItems: 'center' }}>
                 <Button title="Close" onPress={onClose} />
+                {/* <Icon name='keyboard-arrow-down' size={60} color='white' onPress={onClose}/> */}
               </View>
             </View>
           )}
